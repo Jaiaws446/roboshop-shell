60cdb6adf3ded47eb255cc3adf4b6fdc4918b7a3
jaiaws446 <12192jaipal@gmail.com>
2023-12-25 15:07:16 +0530

    mysql

--- shipping.sh
+++ shipping.sh
@@ -29,3 +29,44 @@ then
 else
     echo "you are root user"
 fi #fi means reverse of if, indicating condition end
+
+dnf install maven -y &>> $LOGFILE
+
+VALIDATE $? "Installing maven"
+
+id roboshop
+if [ $? -ne 0 ]
+then 
+    useradd roboshop
+    VALIDATE $? "roboshop user creation"
+else
+    echo -e "roboshop user already exist $Y SKIPPING $N"
+fi     
+
+mkdir -p /app
+
+VALIDATE $? "creating app directory"
+
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip
+
+cd /app
+
+unzip -o /tmp/shipping.zip
+
+mvn clean package
+
+mv target/shipping-1.0.jar shipping.jar
+
+cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
+
+systemctl daemon-reload
+
+systemctl enable shipping 
+
+systemctl start shipping
+
+dnf install mysql -y
+
+mysql -h mysql.jaiaws446.online -uroot -pRoboShop@1 < /app/schema/shipping.sql 
+
+systemctl restart shipping
