247990fb00c74f60ef7becc91ea682e493e8478a
jaiaws446 <12192jaipal@gmail.com>
2023-12-25 15:21:43 +0530

    shipping

--- shipping.sh
+++ shipping.sh
@@ -49,24 +49,48 @@ VALIDATE $? "creating app directory"
 
 curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip
 
+VALIDATE $? "downloading shipping"
+
 cd /app
+
+VALIDATE $? "moving to app directory"
 
 unzip -o /tmp/shipping.zip
 
+VALIDATE $? "unzipping shipping"
+
 mvn clean package
+
+VALIDATE $? "Installing dependencies"
 
 mv target/shipping-1.0.jar shipping.jar
 
+VALIDATE $? "Renaming jar file"
+
 cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
+
+VALIDATE $? "copying shipping service"
 
 systemctl daemon-reload
 
+VALIDATE $? "deamon reload"
+
 systemctl enable shipping 
+
+VALIDATE $? "enable shipping"
 
 systemctl start shipping
 
+VALIDATE $? "Start shipping"
+
 dnf install mysql -y
+
+VALIDATE $? "Installing MYSQL client"
 
 mysql -h mysql.jaiaws446.online -uroot -pRoboShop@1 < /app/schema/shipping.sql 
 
+VALIDATE $? "Loading shipping data"
+
 systemctl restart shipping
+
+VALIDATE $? "Restart shipping"
