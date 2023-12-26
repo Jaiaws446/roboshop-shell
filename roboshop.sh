6a175b3ba94799dd9a1d177791d179c23d557ced
jaiaws446 <12192jaipal@gmail.com>
2023-12-26 14:47:05 +0530

    roboshop instance creation

--- roboshop.sh
+++ roboshop.sh
@@ -10,33 +10,34 @@ DOMAIN_NAME="jaiaws446.online"
 for i in "${INSTANCES[@]}"
 do
 
-    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
-    then
-        INSTANCE_TYPE="t3.small"
-    else
-        INSTANCE_TYPE="t2.micro"
-    fi
-        IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-07f4d0b903a441148 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+      if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+      then
+            INSTANCE_TYPE="t3.small"
+      else
+            INSTANCE_TYPE="t2.micro"
+      fi
+         IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-07f4d0b903a441148 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
         
         echo "$i: $IP_ADDRESS"
 
 
-       aws route53 change-resource-record-sets \
-       --hosted-zone-id $ZONE_ID \
-       --change-batch '
+        aws route53 change-resource-record-sets \
+        --hosted-zone-id $ZONE_ID \
+        --change-batch '
        {
-            "Comment": "Creating a record set for cognito endpoint",
-             "Changes": [{
-             "Action"              : "CREATE",
-             "ResourceRecordSet"  : {
-                "Name"              : "'$i'.'$DOMIAN_NAME'",
-                "Type"             : "A",
-                "TTL"              : 1,
-                 "ResourceRecords"  : [{
+            "Comment": "Creating a record set for cognito endpoint"
+            ,"Changes": [{
+            "Action"              : "CREATE"
+            ,"ResourceRecordSet"  : {
+                "Name"              : "'$i'.'$DOMAIN_NAME'"
+                ,"Type"             : "A"
+                ,"TTL"              : 1
+                ,"ResourceRecords"  : [{
                     "Value"         : "'$IP_ADDRESS'"
-                }]
+               }]
             }
-           }]
-       }
-           '   
+            }]
+        }
+            '
+       
 done 
