aa91ba40a0c4d9ae4801c000ca7545cd84b8d9b3
jaiaws446 <12192jaipal@gmail.com>
2023-12-26 12:52:51 +0530

    roboshop instance creation

--- roboshop.sh
+++ roboshop.sh
@@ -14,6 +14,7 @@ do
     else
         INSTANCE_TYPE="t2.micro"
     fi
-        aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-07f4d0b903a441148
+        aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-07f4d0b903a441148 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+
 
 done 
