3180c5e3ac2cf56ea6865aa25caecf16a776801b
jaiaws446 <12192jaipal@gmail.com>
2023-12-26 13:53:25 +0530

    roboshop instance creation

--- roboshop.sh
+++ roboshop.sh
@@ -23,20 +23,20 @@ do
 
        aws route53 change-resource-record-sets \
        --hosted-zone-id $ZONE_ID \
-       --change-batch '
+       --change-batch "
        {
-            "Comment": "Creating a record set for cognito endpoint"
-            ,"Changes": [{
-             "Action"              : "CREATE"
-            ,"ResourceRecordSet"  : {
-                "Name"              : "'$i'.'$DOMIAN_NAME'"
-                ,"Type"             : "A"
-                ,"TTL"              : 1
-                ,"ResourceRecords"  : [{
-                    "Value"         : "'$IP_ADDRESS'"
+            "Comment": "Creating a record set for cognito endpoint",
+             "Changes": [{
+             "Action"              : "CREATE",
+             "ResourceRecordSet"  : {
+                "Name"              : "$i.$DOMIAN_NAME",
+                "Type"             : "A",
+                "TTL"              : 1,
+                 "ResourceRecords"  : [{
+                    "Value"         : "$IP_ADDRESS"
                 }]
             }
         }]
     }
-     '   
+     "   
 done 
